ebad4b4393b16570323a98eac8056c8f70e6f107
Alice Post <alicep2322@gmail.com>
2019-10-28 09:40:59 -0600

    Test 2 passes

--- lib/server.rb
+++ lib/server.rb
@@ -23,11 +23,27 @@ loop do
   puts request_lines
 
   # Generate the Response
-  puts "Sending response."
-  output = "<html>Hello from the Server side!</html>"
-  status = "http/1.1 200 ok"
-  response = status + "\r\n" + "\r\n" + output
-
+  if request_lines.first == 'GET / HTTP/1.1'
+    puts "Sending response."
+    output = "<html>Hello from the Server side!</html>"
+    status = "http/1.1 200 ok"
+    response = status + "\r\n" + "\r\n" + output
+  elsif request_lines.first == 'POST / HTTP/1.1'
+    puts 'Sending Response'
+    output = "<html>You made a post request</html>"
+    status = "http/1.1 202 ok"
+    response = status + "\r\n" + "\r\n" + output
+  elsif request_lines.first == 'PATCH / HTTP/1.1'
+    puts 'Sending Response'
+    output = "<html>You made a patch request</html>"
+    status = "http/1.1 405 ok"
+    response = status + "\r\n" + "\r\n" + output
+  elsif request_lines.first == 'DELETE / HTTP/1.1'
+    puts 'Sending Response'
+    output = "<html>You made a delete request</html>"
+    status = "http/1.1 401 ok"
+    response = status + "\r\n" + "\r\n" + output
+  end
   # Send the Response
   connection.puts response
 
